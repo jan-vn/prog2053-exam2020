b3c62581f8a3c50128ba5699e84d80c161719aad
Jan Van Ngo <janvn@stud.ntnu.no>
2020-12-17 11:19:01 +0100

    added the edit-user code

--- oppgave/www/js/components/edit-user.js
+++ oppgave/www/js/components/edit-user.js
@@ -7,7 +7,49 @@ class EditUser extends LitElement {
     };
   }
 
-  // din kode her
+  render() {
+    return html`
+    <form onsubmit:"javascript: return false;" id="user" method="POST">
+    <div>
+      <label for="firstName">First Name</label>
+      <input id="firstName" name="firstName" type="text" value="${this.user.firstName}" required>
+    </div>
+    <div>
+      <label for="lastName">Last Name</label>
+      <input id="lastName" name="lastName" type="text" value="${this.user.lastName}" required>
+    </div>
+    <div>
+      <label for="email">E-mail</label>
+      <input id="uname" name="uname" type="text" value="${this.user.uname}" required>
+    </div>
+    <div>
+      <label for="oldPwd">Old Password</label>
+      <input id="oldPwd" name="oldPwd" type="password" value="Old Password" required>
+    </div>
+    <div>
+      <label for="newPwd">New Password</label>
+      <input id="newPwd" name="newPwd" type="password" value="New Password" required>
+    </div>
+     <input type="submit" @click=${this.updateUser} id="submit" name="editUser" value="Submit"></input>
+    </form>
+  `;
+}
+
+updateUser(e) {
+  const data = new editedData(e.target.form);
+  console.log(e);
+  fetch('api/updateUsers.php', {
+    method: 'POST',
+    body: data
+  }).then(res=>res.json()).then(data=>{
+    if (data.status == 'success') {
+      console.log("Sucess: user has been updated");
+    } else {
+      console.log("Error: user has not been updated");
+    }
+  })
+}
 
 }
+
 customElements.define('edit-user', EditUser);
